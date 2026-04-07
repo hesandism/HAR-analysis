9af35dc49bd7b2ee8eb4455d2a8c0bfe29dd8b46
HesandiSM <hesandism@gmail.com>
2026-04-07 10:43:16 +0530

    Add scaling and move the model training into models.py  file

--- models.py
+++ models.py
@@ -3,7 +3,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC, SVC
 from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import time
 
 def get_models():
     models = {
@@ -15,3 +16,32 @@ def get_models():
         "K-Nearest Neighbor": KNeighborsClassifier(n_neighbors=5, n_jobs=-1),
     }
     return models
+
+
+def train_and_evaluate(models, X_train, y_train, X_test, y_test):
+
+    results = []
+
+    trained_models = {}
+
+    for name, model in models.items():
+        print(f"Training {name}...")
+        Xtr =  X_train
+        Xte =  X_test
+        start = time.time()
+        model.fit(Xtr, y_train)
+        end = time.time()
+        training_time = round(end - start, 2)
+        print(f"Trained {name} in {training_time} seconds.")
+        y_pred = model.predict(Xte)
+        trained_models[name] = (model,y_pred)
+
+        results.append({
+            "Model":     name,
+            "Precision": round(precision_score(y_test, y_pred, average='weighted'), 2),
+            "Recall":    round(recall_score   (y_test, y_pred, average='weighted'), 2),
+            "F1-Score":  round(f1_score       (y_test, y_pred, average='weighted'), 2),
+            "Accuracy":  round(accuracy_score (y_test, y_pred), 2),
+            "Training Time": training_time
+        })
+    return results, trained_models
